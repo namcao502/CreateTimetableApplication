2dc04dcc0b3299acaabb24c8c515bd7ea8a71820
quanqvinh <vinh20011118@gmail.com>
2021-06-20 14:52:52 +0700

    Add comment in Program

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -21,6 +21,7 @@ namespace GUI
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+        // Điều chỉnh DPI của máy
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
     }
